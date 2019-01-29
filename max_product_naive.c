86b5b93158bc9c69dab0497a95421c5573777e2b
Iman Anwarzai <imananwarzai@Imans-MacBook-Air.local>
2019-01-28 22:16:08 -0500

    done

--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,10 @@
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
 #include "max_product.h"
 
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+	int max = arr[0];
+	for(int i = 0; i < size; i++)
+		for(int j = 0; j < size; j++)
+			max = MAX(max, arr[i]* arr[j]);
+	return max;
 }
